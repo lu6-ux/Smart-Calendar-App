4cf22ef05fb8fe13388a5e2e08f7ad1c09184b77
Gnanasekaram Lakshana <2023t01877@stu.cmb.ac.lk>
2026-03-10 21:37:58 +0530

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SQLite;
+
+namespace SmartCalendarApp
+{
+    public class Database
+    {
+        public static SQLiteConnection GetConnection()
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=SmartCalendar.db;Version=3;");
+            conn.Open();
+            return conn;
+        }
+    }
+}
