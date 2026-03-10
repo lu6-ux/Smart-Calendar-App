ecc8120786d105926899c3e185938ad016c78f32
Gnanasekaram Lakshana <2023t01877@stu.cmb.ac.lk>
2026-03-10 21:38:17 +0530

    Add files via upload

---
+++ EditEventForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Smart_Calendar_App
+{
+    public partial class EditEventForm : Form
+
+    {
+        public string EventTitle { get; set; }
+        public string EventDate { get; set; }
+        public string EventTime { get; set; }
+        public EditEventForm(string title, string date, string time)
+        {
+            InitializeComponent();
+
+            txtTitle.Text = title;
+            txtDate.Text = date;
+            txtTime.Text = time;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            @EventTitle = txtTitle.Text;
+            @EventDate = txtDate.Text;
+            @EventTime = txtTime.Text;
+
+            
+            this.DialogResult = DialogResult.OK;
+        
+    }
+
+        private void EditEventForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
