442187bdbf47cca75b7250adb97d979e47afaa24
Gnanasekaram Lakshana <2023t01877@stu.cmb.ac.lk>
2026-03-10 21:44:01 +0530

    Add files via upload

---
+++ Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartCalendarApp;
+
+namespace SmartCalendarApp
+{
+    public static class Session
+    {
+        public static string CurrentUser { get; set; }
+    }
+}
