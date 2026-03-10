377b879006d7203efe6702dc1778ba2fcdab3f75
Gnanasekaram Lakshana <2023t01877@stu.cmb.ac.lk>
2026-03-10 21:40:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using SmartCalendarApp;
+
+namespace Smart_Calendar_App
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (LoginForm loginForm = new LoginForm())
+            {
+                if (loginForm.ShowDialog() == DialogResult.OK) // Only open main if login succeeds
+                {
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
+
+
+
