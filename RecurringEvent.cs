5b92f9f50ca861e302055baf98ed3ebe54b870fe
Gnanasekaram Lakshana <2023t01877@stu.cmb.ac.lk>
2026-03-10 21:41:36 +0530

    Add files via upload

---
+++ RecurringEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Smart_Calendar_App.Models
+{
+    public class RecurringEvent
+    {
+        public int RecurringID { get; set; }         
+        public int EventID { get; set; }             
+        public string RecurrenceType { get; set; }   
+        public DateTime? RecurrenceEndDate { get; set; }  
+        public int? RecurrenceCount { get; set; }         
+        public DateTime CreatedAt { get; set; }      
+    }
+}
+
